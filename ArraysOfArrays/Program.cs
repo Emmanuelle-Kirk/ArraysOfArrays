0f0fa266c9093c61943c6c905f7d3b2ca6670eb4
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-25 12:51:01 -0300

    oooooarrays

--- ArraysOfArrays/Program.cs
+++ ArraysOfArrays/Program.cs
@@ -10,6 +10,35 @@ namespace ArraysOfArrays
     {
         static void Main(string[] args)
         {
+            //int score = 0;
+            
+          //  int x;
+           // int y; // x and y is location
+
+           // int weapon; //player weapon
+
+            string title = "Coolest Game Ever";
+            Console.WriteLine("Single-dimensional Arrays");
+            Console.WriteLine();
+
+            int maxPlayers = 4;
+
+            int[] score = new int[4];
+            int[] x = new int[4];
+            int[] y = new int[4];
+            int[] weapon = new int[4];
+
+            for (int player = 0; player < maxPlayers; player++)
+            {
+                Console.WriteLine("Player #" + (player + 1) + " Score: " + score[player]);
+            }
+
+
+            //init. scores to 0
+            //init. player locations to something??
+            //init. player weapons to 0
+
+
             Console.WriteLine("Multi-dimensional Arrays");
             Console.WriteLine();
 
