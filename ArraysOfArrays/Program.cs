affc1ef26d60e71fd9e573eca05ebf8deff13f8f
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-25 12:21:58 -0300

    Initial commit

---
+++ ArraysOfArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysOfArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multi-dimensional Arrays");
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+        }
+    }
+}
